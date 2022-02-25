d42f463b5f892baf617798d9625c2b47e478f40a
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-02-25 16:21:29 +0000

    My second JavaScript Project

---
+++ index.js
@@ -0,0 +1,53 @@
+let valuee = document.getElementById("values");
+
+let one = 1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let seven = 7;
+let eight = 8;
+let nine = 9;
+let zero = 0;
+document.getElementById("values").textContent;
+
+function numOne() {
+  valuee.textContent = one;
+}
+
+function numTwo() {
+  valuee.textContent = two;
+}
+
+function numThree() {
+  valuee.textContent = three;
+}
+
+function numFour() {
+  valuee.textContent = four;
+}
+
+function numFive() {
+  valuee.textContent = five;
+}
+
+function numSix() {
+  valuee.textContent = six;
+}
+
+function numSeven() {
+  valuee.textContent = seven;
+}
+
+function numEight() {
+  valuee.textContent = eight;
+}
+
+function numNine() {
+  valuee.textContent = nine;
+}
+
+function numZero() {
+  valuee.textContent = zero;
+}
